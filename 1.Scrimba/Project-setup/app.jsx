6a733d902f74e3a5c81443f9bc98f44ef6451f6f
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-02-14 21:19:06 +0600

    Initialize project structure with App, Main, and Nav components; add index.html and style.css

---
+++ 1.Scrimba/Project-setup/app.jsx
@@ -0,0 +1,11 @@
+import Main from "./componants/Main"
+import Nav from "./componants/Nav"
+
+export default function App() {
+    return (
+        <>
+          <Nav/>
+          <Main/>
+        </>
+    )
+}
