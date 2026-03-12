dc6fb7e939a36db267925323e7ae363cc54210c3
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-03-13 00:10:13 +0600

    feat: implement Pad component with toggle functionality and update App component to use it

--- 1.Scrimba/Sound-pads/src/App.jsx
+++ 1.Scrimba/Sound-pads/src/App.jsx
@@ -1,15 +1,15 @@
 // part1
 
 
-import pads from "./pads.js"
-import React from "react"
+// import pads from "./pads.js"
+// import React from "react"
 
-export default function App({ darkMode }) {
+// export default function App({ darkMode }) {
     // console.log(darkMode)
-    const [padsArr, setPads] = React.useState(pads)
+    // const [padsArr, setPads] = React.useState(pads)
 
     // way 1
-    const buttonElement = padsArr.map(padItem => <button style={{ backgroundColor: darkMode ? "#222222" : "#cccccc", }} key={padItem.id}></button>)
+    // const buttonElement = padsArr.map(padItem => <button style={{ backgroundColor: darkMode ? "#222222" : "#cccccc", }} key={padItem.id}></button>)
 
     // way2
     // const styles = {
@@ -18,17 +18,61 @@ export default function App({ darkMode }) {
 
     // const buttonElement = padsArr.map(item => <button style={styles} key={item.id}></button>) 
 
-    return (
-        <main>
-            <div className="pad-container">
-                {buttonElement}
-            </div>
-        </main>
-    )
-}
+//     return (
+//         <main>
+//             <div className="pad-container">
+//                 {buttonElement}
+//             </div>
+//         </main>
+//     )
+// }
 /**
    * Challenge: use a ternary to determine the backgroundColor
    * of the buttons
    * If darkMode is true, set them to "#222222"
    * If darkMode is false, set them to "#cccccc"
    */
+
+
+// part2
+// import React from "react"
+// import padsData from "./pads"
+// import Pad from "./Pad"
+
+// export default function App() {
+//     const [pads, setPads] = React.useState(padsData)
+
+//     const buttonElements = pads.map(pad => (
+//         <Pad key={pad.id} color={pad.color} on={pad.on}/>
+//     ))
+    
+//     return (
+//         <main>
+//             <div className="pad-container">
+//                 {buttonElements}
+//             </div>
+//         </main>
+//     )
+// }
+
+
+// part3
+import React from "react"
+import padsData from "./pads"
+import Pad from "./Pad"
+
+export default function App() {
+    const [pads, setPads] = React.useState(padsData)
+
+    const buttonElements = pads.map(pad => (
+        <Pad key={pad.id} color={pad.color} on={pad.on} />
+    ))
+    
+    return (
+        <main>
+            <div className="pad-container">
+                {buttonElements}
+            </div>
+        </main>
+    )
+}
---
+++ 1.Scrimba/Sound-pads/src/Pad.jsx
@@ -0,0 +1,29 @@
+// export default function Pad(props) {
+
+//     return (
+//         <button 
+//             style={{backgroundColor: props.color}}
+//             className={props.on ? "on" : ""}
+//         ></button>
+//     )
+// }
+
+
+
+// part3
+import React from "react"
+export default function Pad(props) {
+    console.log(props.on)
+    const [isTrue, setTrue] = React.useState(props.on)
+    function flip() {
+        setTrue(pre => !pre)
+    }
+
+    return (
+        <button
+            style={{ backgroundColor: props.color }}
+            className={isTrue ? "on" : ""}
+            onClick={flip}
+        ></button>
+    )
+}
